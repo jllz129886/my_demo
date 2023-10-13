a055071419717e448dac2e02d633f6451f33752b
jllz129886 <144105786+jllz129886@users.noreply.github.com>
2023-10-13 16:36:14 +0800

    Update cytq.js

--- cytq.js
+++ cytq.js
@@ -1,49 +1,77 @@
 /*
 
-iTunes解锁
+ 彩云天气
 
-[rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/jllz129886/my_demo/main/iTunes.js
+ [rewrite_local]
+ ^https?:\/\/(biz|wrapper)\.cyapi\.cn\/(.+\/(user.+|visitors|activity)|p\/v\d\/vip_info) url script-response-body https://raw.githubusercontent.com/jllz129886/my_demo/main/cytq.js
+ ^http:\/\/adx\.sogaha\.cn\/sdk\/ad\/get url reject-200
 
-[MITM]
-hostname = buy.itunes.apple.com
+ [mitm]
+ hostname = *.cyapi.cn, adx.sogaha.cn
 
 */
-var youXiu = {};
-var youXiu1 =JSON.parse(typeof $response != "undefined" && $response.body || null);
-// var product_id = "com.photoslab.ai.writerassistant.year";
-var product_id = "caiyun.colorfulclouds.pro.pay.continuous_yearly_svip";
-var receipt ={
-        "quantity" : "1",
-        "purchase_date_ms" : "1693976946000",
-        "expires_date" : "2099-12-31 23:59:59 Etc/GMT",
-        "expires_date_pst" : "2099-12-31 23:59:59 America/Los_Angeles",
-        "is_in_intro_offer_period" : "false",
-        "transaction_id" : "490001314520000",
-        "is_trial_period" : "false",
-        "original_transaction_id" : "490001314520000",
-        "purchase_date" : "2023-09-06 13:09:06 Etc/GMT",
-        "product_id" : product_id,
-        "original_purchase_date_pst" : "2023-09-06 13:09:06 America/Los_Angeles",
-        "in_app_ownership_type" : "PURCHASED",
-        "original_purchase_date_ms" : "1693976946000",
-        "web_order_line_item_id" : "490000123456789",
-        "expires_date_ms" : "4102415999999",
-        "purchase_date_pst" : "2023-09-06 13:09:06 America/Los_Angeles",
-        "original_purchase_date" : "2023-09-06 13:09:06 Etc/GMT"
-}
-var renewal = {
-    "expiration_intent": "1",
-    "product_id": product_id,
-    "is_in_billing_retry_period": "0",
-    "original_transaction_id" : "490001314520000",
-    "auto_renew_product_id" : product_id,
-    "auto_renew_status" : "1"
-}
 
-youXiu1.receipt.in_app = [receipt];
-youXiu1.latest_receipt_info = [receipt];
-youXiu1.pending_renewal_info = [renewal];
 
-youXiu = youXiu1;
-$done({ body: JSON.stringify(youXiu) });
+var youXiu = JSON.parse($response.body);
+const urls = ['/vip_info', '/activity', 'user'];
+
+urls.forEach(url => {
+    if ($request.url.includes(url)){
+        switch(url) {
+            case '/vip_info':
+                youXiu = {
+                    ...youXiu,
+                    vip: {
+                        "expires_time" : "4102415999",
+                        "is_auto_renewal" : true
+                    },
+                    svip: {
+                        "expires_time" : "4102415999",
+                        "is_auto_renewal" : true
+                    }
+                }
+                break;
+            case '/activity':
+                youXiu.activities = [];
+                break;
+            case 'user':
+                youXiu.result = {
+                    ...youXiu.result,
+                    ranking_above: 99,
+                    is_vip: true,
+                    vip_expired_at: 4102415999,
+                    svip_given: 9999,
+                    is_xy_vip: true,
+                    xy_svip_expire: 4102415999,
+                    wt: {
+                        ...youXiu.result.wt,
+                        vip: {
+                            "auto_renewal_type" : "",
+                            "expired_at" : 0,
+                            "enabled" : true,
+                            "svip_apple_expired_at" : 4102415999,
+                            "is_auto_renewal" : true,
+                            "svip_expired_at" : 4102415999,
+                            "svip_auto_renewal_type" : ""
+                        },
+                        svip_given: 9999,
+                        ranking_above: 99,
+                    },
+                    is_phone_verified: true,
+                    phone_num: "13145200000",
+                    vip_take_effect: 1,
+                    is_primary: true,
+                    xy_vip_expire: 4102415999,
+                    svip_expired_at: 4102415999,
+                    svip_take_effect: 1,
+                    vip_type: "s",
+                    name: "优秀",
+                    avatar: "http://img.crcz.com/allimg/202001/21/1579536384677793.jpg",
+                    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoiNjUyMWY3OGQwNDFlNjIwMDE4MDU3YzYyIiwidXNlcl9pZCI6IjY1MDFhMzkxMjQ0NWI5MDAxOGQxYjBlMCIsInZlcnNpb24iOjIsImV4cCI6MTY5OTcxOTg1OCwidmlwX2V4cGlyZWRfYXQiOjAsImlzcyI6IndlYXRoZXIiLCJpYXQiOjE2OTcxMjc4NTgsInN2aXBfZXhwaXJlZF9hdCI6MCwicHJpbWFyeSI6dHJ1ZX0.U6AkP3HcOzP5kd49nXGV71NC4VrPe57ZQqSfyCG-Dv0"
+                }
+                break;
+        }
+    }
+})
+
+$done({ body: JSON.stringify(youXiu)});
